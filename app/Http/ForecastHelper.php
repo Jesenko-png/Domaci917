c7cb4dc7e59051941b62c0ff9441b9eba16362f8
Jesenko-png <jesenko9@hotmail.com>
2026-05-14 03:56:45 +0200

    php helper set up

---
+++ app/Http/ForecastHelper.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http;
+
+class ForecastHelper
+{
+    public static function getColorByTemperature($temperature){
+
+        if ($temperature <= 0)
+        {
+            $boja = "lightblue";
+        }
+        elseif ($temperature >=1 && $temperature <= 15)
+        {
+            $boja="blue";
+        }
+        elseif ($temperature > 15 && $temperature <= 25){
+            $boja="green";
+        }
+        else{
+            $boja="red";
+        }
+        return $boja;
+    }
+}
--- resources/views/admin/forecast_store.blade.php
+++ resources/views/admin/forecast_store.blade.php
@@ -1,24 +1,26 @@
+@php use App\Http\ForecastHelper; @endphp
+
 <form action="{{route("forecast.store")}}" method="post">
     @csrf
 
-        <select name="city_id" >
-            @foreach(\App\Models\CitiesModel::all() as $city)
+    <select name="city_id">
+        @foreach(\App\Models\CitiesModel::all() as $city)
             <option value="{{$city->id}}">
                 {{$city->name}}
             </option>
-            @endforeach
+        @endforeach
 
-        </select>
+    </select>
     <input type="text" name="temperature" placeholder="Unesite vasu temperaturu">
     <select name="weather_type">
 
-           @foreach(\App\Models\ForecastModel::WEATHERS as $type)
+        @foreach(\App\Models\ForecastModel::WEATHERS as $type)
             <option value="{{$type}}">
                 {{$type}}
             </option>
-           @endforeach
-  </select>
-    <input type="number" name="sansa" placeholder="Unesite sansu za padavinu" >%
+        @endforeach
+    </select>
+    <input type="number" name="sansa" placeholder="Unesite sansu za padavinu">%
 
     <input type="date" name="forecast_date">
     <button type="submit">Snimi</button>
@@ -27,35 +29,15 @@
 @foreach(\App\Models\CitiesModel::all() as $city)
     <h3>{{$city->name}}</h3>
 
-        <ul>
-            @foreach($city->forecasts as $forecast)
-                @php
-                    $boja;
-                    if ($forecast->temperature <= 0)
-                        {
-                            $boja = "lightblue";
-                        }
-                    elseif ($forecast->temperature >=1 && $forecast->temperature <= 15)
-                {
-                    $boja="blue";
-                }
-                    elseif ($forecast->temperature > 15 && $forecast->temperature <= 25){
-                        $boja="green";
-                    }
-                    else{
-                        $boja="red";
-                    }
+    <ul>
+        @foreach($city->forecasts as $forecast)
+            @php
+                $boja=ForecastHelper::getColorByTemperature($forecast->temperature)
+            @endphp
+            <li>{{$forecast->forecast_date}}-<span style="color:{{$boja}};">{{$forecast->temperature}}</span></li>
 
-                 @endphp
-
-
-
-
-
-        <li>{{$forecast->forecast_date}}-<span style="color:{{$boja}};">{{$forecast->temperature}}</span></li>
-
-            @endforeach
-        </ul>
+        @endforeach
+    </ul>
 
 @endforeach
 
